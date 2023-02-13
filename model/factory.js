6f9671dd0ea390d0cf87f0f2927ca063d95758db
sanka01 <sanka1375@gmail.com>
2023-02-12 23:07:54 -0300

    .

--- model/factory.js
+++ model/factory.js
@@ -48,6 +48,30 @@ const Factory = () => {
     const [coins, setCoins] = React.useState(0);
     const [clickValue, setClickValue] = React.useState(1);
     const [autoClicks, setAutoClicks] = React.useState(0);
+    const [clickUpgradePrice, setClickUpgradePrice] = React.useState(10);
+    const [autoClickUpgradePrice, setAutoClickUpgradePrice] = React.useState(100);
+    const [clickUpgrades, setClickUpgrades] = React.useState(0);
+    const [autoClickUpgrades, setAutoClickUpgrades] = React.useState(0);
+
+    const calculateUpgradePrice = (basePrice, upgrades) => basePrice * (1.1 ** upgrades);
+
+    const upgradeClick = () => {
+        if (coins >= clickUpgradePrice) {
+            setCoins(coins - clickUpgradePrice);
+            setClickUpgrades(clickUpgrades + 1);
+            setClickValue(clickValue + 1);
+            setClickUpgradePrice(calculateUpgradePrice(10, clickUpgrades + 1));
+        }
+    };
+
+    const upgradeAutoClick = () => {
+        if (coins >= autoClickUpgradePrice) {
+            setCoins(coins - autoClickUpgradePrice);
+            setAutoClickUpgrades(autoClickUpgrades + 1);
+            setAutoClicks(autoClicks + 1);
+            setAutoClickUpgradePrice(calculateUpgradePrice(100, autoClickUpgrades + 1));
+        }
+    };
 
     const addCoins = (value) => {
         setCoins(coins + value);
@@ -57,44 +81,32 @@ const Factory = () => {
         addCoins(clickValue);
     };
 
-    const buyUpgrade = (upgradeType) => {
-        if (upgradeType === 'click') {
-            if (coins >= 10) {
-                setClickValue(clickValue + 1);
-                setCoins(coins - 10);
-            }
-        } else if (upgradeType === 'auto') {
-            if (coins >= 50) {
-                setAutoClicks(autoClicks + 1);
-                setCoins(coins - 50);
-            }
-        }
-    };
     React.useEffect(() => {
         if (autoClicks > 0) {
-          const autoClick = () => {
-            setCoins(coins => coins + autoClicks);
-            setTimeout(autoClick, 1000);
-          };
-          autoClick();
+            const autoClick = () => {
+                setCoins(coins => coins + autoClicks);
+                setTimeout(autoClick, 1000);
+            };
+            autoClick();
         }
-      }, [autoClicks]);
-      
-      
-    
+    }, [autoClicks]);
+
+
+
     return (
         <View style={styles.container}>
             <TouchableOpacity style={styles.factoryContainer} onPress={handleClick}>
                 <Image source={factoryImage} style={{ height: 100, width: 100 }} />
             </TouchableOpacity>
-            <Text style={styles.coinsText}>Coins: {coins}</Text>
+            <Text style={styles.coinsText}>Coins: {coins.toFixed(0)}</Text>
             <Text style={styles.clickValueText}>Click Value: {clickValue}</Text>
             <Text style={styles.autoClicksText}>Auto Clicks: {autoClicks}</Text>
-            <TouchableOpacity style={styles.upgradeButton} onPress={() => buyUpgrade('click')}>
-                <Text>Upgrade Click</Text>
+
+            <TouchableOpacity style={styles.upgradeButton} onPress={() => upgradeClick()}>
+                <Text>Upgrade Click: {clickUpgradePrice.toFixed(0)} coins</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.upgradeButton} onPress={() => buyUpgrade('auto')}>
-                <Text>Upgrade Auto Click</Text>
+            <TouchableOpacity style={styles.upgradeButton} onPress={() => upgradeAutoClick()}>
+                <Text>Upgrade Auto Click: {autoClickUpgradePrice.toFixed(0)} coins </Text>
             </TouchableOpacity>
         </View>
     );
